d92b57638474eb7d14ffee516eab79a57605d9cf
Abdul Ahad Siddiqui <abdulahadsiddiqui011@gmail.com>
2022-11-20 23:56:25 +0530

    VideoCard component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,35 @@
+import type { NextPage } from 'next';
+import axios from 'axios';
 
-export default function Home() {
-  return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
+import { Video } from '../types';
+import VideoCard from '../components/VideoCard';
+import NoResults from '../components/NoResults';
+
+interface IProps {
+  videos: Video[];
 }
+
+const Home = ({ videos }: IProps) => {
+  return (
+    <div className='flex flex-col gap-10 videos h-full'>
+      {videos.length ? (
+        videos.map((video: Video) => (
+          <VideoCard post = {video} key = {video._id}/>
+        ))
+      ) : (
+        <NoResults text = {'No Videos'}/>
+      )}
+    </div>
+  );
+};
+
+export const getServerSideProps = async () => {
+  const { data }= await axios.get(`http://localhost:3000/api/post`);
+  
+  return {
+    props: {
+      videos: data
+    },
+  }
+}
+export default Home;
--- utils/client.ts
+++ utils/client.ts
@@ -1,7 +1,7 @@
 import sanityClient from '@sanity/client';
 
 export const client = sanityClient({
-  projectId: 'tp4ln3j8',
+  projectId: 'eu6midyj',
   dataset: 'production',
   apiVersion: '2022-03-10',
   useCdn: false,
