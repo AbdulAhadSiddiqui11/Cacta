dff6a879ce0a007b7b80b62c272c3e09cf796cfd
Abdul Ahad Siddiqui <abdulahadsiddiqui011@gmail.com>
2022-11-26 19:45:34 +0530

    Profile page

--- pages/profile/[id].tsx
+++ pages/profile/[id].tsx
@@ -16,17 +16,70 @@ interface IProps {
   }
 }
 
-const profile = ({ data }: IProps) => {
+const Profile = ({ data }: IProps) => {
+  const [showUserVideos, setShowUserVideos] = useState(true);
+  const [videosList, setVideosList] = useState<Video[]>([]);
+  const { user, userVideos, userLikedVideos } = data;
+  
+  const videos =  showUserVideos ? 'border-b-2 border-black' : 'text-gray-400';
+  const liked =  !showUserVideos ? 'border-b-2 border-black' : 'text-gray-400';
+
+  useEffect(() => {
+    if(showUserVideos) {
+      setVideosList(userVideos);
+    } else {
+      setVideosList(userLikedVideos);
+    }
+  }, [showUserVideos, userVideos, userLikedVideos]);
+
   return (
-    <div>profile</div>
+    <div className='w-full'>
+      <div className='flex gap-6 md:gap-10 mb-4 bg-white w-full'>
+        <div className='w-16 h-16 md:w-32 md:h-32 '>
+            <Image 
+              src={user.image}
+              width={120}
+              height={120}
+              className='rounded-full'
+              alt='User Profile'
+              layout='responsive'
+            />
+        </div>
+        <div className='flex flex-col justify-center'>
+          <p className='md:text-2xl tracking-wider flex gap-1 items-ceter justify-center text-md font-bold text-primary lowercase'>
+            {user.userName.replaceAll(' ', '')}
+            <VerifiedIcon className='text-blue-400'/>
+          </p>
+          <p className='capitalize md:text-xl text-gray-400 text-xs'>
+            {user.userName}
+          </p>
+        </div>
+      </div> 
+
+      <div>
+        <div className='flex gap-10 mb-10 mt-10 border-b-2 border-gray-200 bg-white w-full'>
+          <p className={`text-xl font-semibold cursor-pointer mt-2 ${videos}`} onClick={() => setShowUserVideos(true)}>
+            Videos
+          </p>
+          <p className={`text-xl font-semibold cursor-pointer mt-2 ${liked}`} onClick={() => setShowUserVideos(false)}>
+            Liked Videos
+          </p>
+        </div>
+        <div className='flex gap-6 flex-wrap md:justify-start'>
+          {videosList.length > 0 ? ( videosList.map((post: Video, idx: number) => 
+            ( <VideoCard post={post} key={idx} />))
+          ) : <NoResults text={`No ${showUserVideos ? '' : 'Liked'} Videos Yet`}/>}
+        </div>
+      </div>
+    </div>
   )
 }
 
-export const getServersideProps = async ({params: { id }}: {params: {id: string}}) => {
+export const getServerSideProps = async ({params: { id }}: {params: {id: string}}) => {
   const res = await axios.get(`${BASE_URL}/api/profile/${id}`);
   return {
     props: {data: res.data}
   }
 }
 
-export default profile;
+export default Profile;
