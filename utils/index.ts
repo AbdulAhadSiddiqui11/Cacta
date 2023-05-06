c8f9fec73281744d1049c237565217dbc1c31122
Abdul Ahad Siddiqui <abdulahadsiddiqui011@gmail.com>
2023-05-06 16:42:53 +0530

    CROS base URL update

--- utils/index.ts
+++ utils/index.ts
@@ -1,7 +1,8 @@
 import axios from 'axios';
 import jwt_decode from 'jwt-decode';
 
-export const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
+//export const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
+export const BASE_URL = window.location.origin;
 
 export const createOrGetUser = async (response: any, addUser: any) => {
     const decoded: {name: string, picture: string, sub: string} = jwt_decode(response.credential);
